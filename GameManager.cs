bba7a48dff4901937f914bec05c25a9f93c34f2d
Robb Crouse <34464875+RobbCrouse@users.noreply.github.com>
2018-04-05 15:32:31 -0700

    Add files via upload
    Added PlayGame and AddToDiscardPile methods

--- GameManager.cs
+++ GameManager.cs
@@ -43,7 +43,84 @@ namespace UNO.GameObjects{
         }
 
         ///$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+        public void PlayGame(){
+            int i = 0;
+            bool isAscending = true;
 
+            //Show what each player starts with
+            foreach ( var player in Players){
+                player.ShowHand();
+            }
+
+            Console.ReadLine();
+
+            PlayerTurn currentTurn = new PlayerTurn(){
+                Result = TurnResult.GameStart,
+                Card = DiscardPile.First(),
+                DeclaredColor = DiscardPile.First().Color
+            };
+
+            Console.WriteLine("First card is a " + currentTurn.Card.DisplayValue + ".");
+
+            while( !Players.Any( x => !x.Hand.Any())){
+                if( DrawPile.Cards.Count < 4){ //small cheat here
+                    var currentCard = DiscardPile.First();
+
+                    //Take the discarded cards, shuffle them, and make them the new draw pile.
+                    DrawPile.Cards = DiscardPile.Skip(1).ToList();
+                    DrawPile.Shuffle();
+
+                    //Reset the discard pile to only have the current card.
+                    DiscardPile = new List<Card>();
+                    DiscardPile.Add(currentCard);
+
+                    Console.WriteLine("Shuffling Cards!");
+                }
+
+                var currentPlayer = Players[i];
+
+                currentTurn = Players[i].PlayTurn( currentTurn, DrawPile);
+
+                AddToDiscardPile( currentTurn);
+
+                if ( currentTurn.Result == TurnResult.Reversed){
+                    isAscending = !isAscending;
+                }
+
+                if ( isAscending){
+                    i++;
+                    if ( i >= Players.Count){ //Reset Player counter
+                        i = 0;
+                    }
+                }
+                else{
+                    i--;
+                    if ( i < 0){
+                        i = Players.Count - 1;
+                    }
+                }
+            }
+
+            var winningPlayer = Players.Where( x => !x.Hand.Any()).First();
+            Console.WriteLine("Player " + winningPlayer.Position.ToString() + " wins!!");
+
+            foreach( var player in Players){
+                
+                Console.WriteLine("Player " + Players.Position.ToString() + " has " + player.Hand.Sum( x => x.Score).ToString() + " points in their hand.");
+            }
+        }
+        private void AddToDiscardPile(PlayerTurn currentTurn)
+        {
+            if (currentTurn.Result == TurnResult.PlayedCard
+                    || currentTurn.Result == TurnResult.DrawTwo
+                    || currentTurn.Result == TurnResult.Skip
+                    || currentTurn.Result == TurnResult.WildCard
+                    || currentTurn.Result == TurnResult.WildDrawFour
+                    || currentTurn.Result == TurnResult.Reversed)
+            {
+                DiscardPile.Insert(0, currentTurn.Card);
+            }
+        }
 
 
     }
